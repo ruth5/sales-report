31a93f103b58ec80b7d4f409d66be6a400f0fdf2
Ruth Rosenthal <ruthbrosenthal@gmail.com>
2021-08-03 04:34:49 +0000

    moved code into two separate functions

--- sales_report.py
+++ sales_report.py
@@ -33,30 +33,36 @@ for line in f:
 for i in range(len(salespeople)):
     print(f'{salespeople[i]} sold {melons_sold[i]} melons')
 
+print('--------------------------------------------')
 # improved code: use a dictionary instead of two lists. use .get() to replace need for conditional
 """Generate sales report showing total melons each salesperson sold."""
 
-# dictionary where salesperson will be key, melons sold will be value
-melon_sales = {}
+def get_sales_info(filename):
+    """ Takes in a filename and returns a dictionary with salespeople as keys and melons sold as values
+    File is a text file with each line being a order with salesperson, amount paid, and melons sold separated by | """
+    # dictionary where salesperson will be key, melons sold will be value
+    melon_sales = {}
 
-# make a file object "f" from the sales report file
-f = open('sales-report.txt')
-# iterate over each line in the file (each order)
-for line in f:
-    # get rid of extra whitespace/new lines, etc.
-    line = line.rstrip()
-    # split up the line by '|' character and put each element in a list called entries
-    entries = line.split('|')
-    # grab the first of the entries, this is the salesperson
-    salesperson = entries[0]
-    # grab the last of the entries, this is the number of melons sold. cast as int
-    melons = int(entries[2])
+    # make a file object "f" from the sales report file
+    with open(filename) as f:
+    # iterate over each line in the file (each order)
+        for line in f:
+            # get rid of extra whitespace/new lines, etc.
+            line = line.rstrip()
+            # split up the line by '|' character
+            salesperson, payment, melons = line.split('|')
 
-    melon_sales[salesperson] = melonsales.get(salesperson, 0) + melons
+            melon_sales[salesperson] = melon_sales.get(salesperson, 0) + int(melons)
 
+    return melon_sales
 
+def print_sales_report(melon_sales):
+    """ Takes in a dictionary with {salesperson:total melons sold} and prints out sales report. """
 # print the number of melons sold by each sales person
-print()
-for salesperson, melons in melon_sales.values():
-    print(f'{salesperson} sold {melons]} melons')
+    for salesperson, melons in melon_sales.items():
+        print(f'{salesperson} sold {melons} melons')
+
+melon_sales = get_sales_info('sales-report.txt')
+print_sales_report(melon_sales)
+
 
